c3648869610fe8c4512231e92e2c9238744e6f1a
Eric Huang <yifengh3@uci.edu>
2021-07-07 07:32:41 -0700

    study_b 1

---
+++ my_Train_B.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Train on B-jets
+
+# In[2]:
+
+
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
+import tensorflow.keras as keras
+import tensorflow.keras.backend as K
+
+import os
+import os.path as osp
+import sys
+
+import numpy as np
+#from scipy import linalg as LA
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+from utils.tf_sinkhorn import ground_distance_tf_nograd, sinkhorn_knopp_tf_scaling_stabilized_class
+import utils.VAE_model_tools
+from utils.VAE_model_tools import build_and_compile_annealing_vae, betaVAEModel, reset_metrics
+
+import pandas
+import matplotlib.pyplot as plt
+
+import h5py
+import pickle
+
+
+# In[4]:
+
+
+def create_dir(dir_path):
+    ''' Creates a directory (or nested directories) if they don't exist.
+    '''
+    if not osp.exists(dir_path):
+        os.makedirs(dir_path)
+
+    return dir_path
+
+# path to file
+fn =  '/global/home/users/yifengh3/data/B_background.h5'
+
+# Option 1: Load everything into memory
+df = pandas.read_hdf(fn,stop=1000000)
+print(df.shape)
+print("Memory in GB:",sum(df.memory_usage(deep=True)) / (1024**3)+sum(df.memory_usage(deep=True)) / (1024**3))
+
+
+# In[9]:
+
+
+# Data file contains, for each event, 50 particles (with zero padding), each particle with pT, eta, phi
+data = df.values.reshape((-1,50,3))
+
+# Normalize pTs so that HT = 1
+HT = np.sum(data[:,:,0],axis=-1)
+data[:,:,0] = data[:,:,0]/HT[:,None]
+# Energy column DNE here, so dont need to normalize the data
+# data[:,:,-1] = data[:,:,-1]/HT[:,None]
+
+# Center jet (optional)
+# cant center the jets since E missing here
+# data = center_jets_ptetaphiE(data)
+
+# Inputs x to NN will be: pT, eta, cos(phi), sin(phi), log E
+# Separated phi into cos and sin for continuity around full detector, so make things easier for NN.
+# Also adding the log E is mainly because it seems like it should make things easier for NN, since there is an exponential spread in particle energies.
+# Feel free to change these choices as desired. E.g. px, py might be equally as good as pt, sin, cos.
+sig_input = np.zeros((len(data),50,4))
+sig_input[:,:,:2] = data[:,:,:2]
+sig_input[:,:,2] = np.cos(data[:,:,2])
+sig_input[:,:,3] = np.sin(data[:,:,2])
+# no input from energy for B jets
+# sig_input[:,:,4] = np.log(data[:,:,3]+1e-8)
+
+
+data_x = sig_input
+# Event 'labels' y are [pT, eta, phi], which is used to calculate EMD to output which is also pT, eta, phi.
+data_y = data
+
+
+train_x = data_x[:80000]
+train_y = data_y[:80000]
+valid_x = data_x[80000:]
+valid_y = data_y[80000:]
+
+
+# In[10]:
+
+
+output_dir = './output/'
+
+experiment_name = 'B_train_1'
+train_output_dir = create_dir(osp.join(output_dir, experiment_name))
+vae, encoder, decoder = build_and_compile_annealing_vae(optimizer=keras.optimizers.Adam(lr=0.001,clipnorm=0.1),
+                                    encoder_conv_layers = [1024,1024,1028,1024],
+                                    dense_size = [1028,1028,1028,512],
+                                    decoder = [2048,2048,1028,512,512],
+                                    numItermaxinner = 40,   # EMD approximation params
+                                    numIter=10,
+                                    reg_init = 1.,
+                                    reg_final = 0.01,
+                                    stopThr=1e-3,
+                                    num_inputs=4,           # Size of x (e.g. pT, eta, sin, cos, log E)
+                                    num_particles_in=50)    # Num particles per event.
+
+batch_size=150
+save_period=2
+
+reduceLR = keras.callbacks.ReduceLROnPlateau(monitor='loss', factor=0.1, patience=2, verbose=1, mode='auto', min_delta=1e-4, cooldown=0, min_lr=0)
+modelcheckpoint = keras.callbacks.ModelCheckpoint(train_output_dir + '/model_weights_{epoch:02d}.hdf5', save_freq = save_period*5000, save_weights_only=True)
+reset_metrics_inst = reset_metrics()
+
+callbacks=[tf.keras.callbacks.CSVLogger(train_output_dir + '/log.csv', separator=",", append=True),
+            reduceLR,
+            modelcheckpoint,
+            reset_metrics_inst]
+
+import json
+import keras
+# generate the model arg file
+vae_args_file = "vae_args.dat"
+vae_arg_dict = {
+                "encoder_conv_layers" : [1024,1024,1028,1024],
+                "dense_size" :[1028,1028,1028,512],
+                "decoder" : [2048,2048,1028,512,512],
+                "numItermaxinner" : 40,   # EMD approximation params
+                "numIter":10,
+                "reg_init" : 1.,
+                "reg_final" : 0.01,
+                "stopThr":1e-3,
+                "num_inputs":4,           # Size of x (e.g. pT, eta, sin, cos, log E)
+                "num_particles_in":50}
+
+
+with open(train_output_dir+vae_args_file,'w') as file:
+  file.write(json.dumps(vae_arg_dict))
+
+
+# Need to train on at least one example before model params can be loaded for annoying reasons.
+
+history = vae.fit(x=train_x[:10], y=train_y[:10], batch_size=batch_size,
+                epochs=1,verbose=1,#initial_epoch=int(vae.optimizer.iterations/numbatches),
+                validation_data = (valid_x[:10],valid_y[:10]),
+                callbacks = callbacks
+              )
+
+
+# In[11]:
+
+
+init_epoch = 0
+steps_per_epoch = 1000
+save_period = 20
+
+
+# reduceLR = keras.callbacks.ReduceLROnPlateau(monitor='val_val_loss', factor=np.sqrt(0.1), patience=5, verbose=1, mode='auto', min_delta=1e-4, cooldown=0, min_lr=1e-8)
+# earlystop = tf.keras.callbacks.EarlyStopping(
+#     monitor='val_val_loss', min_delta=0., patience=10, verbose=0, mode='auto',
+#     baseline=None, restore_best_weights=False
+# )
+
+reduceLR = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), patience=5, verbose=1, mode='auto', min_delta=1e-4, cooldown=0, min_lr=1e-8)
+earlystop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=0., patience=5, verbose=0, mode='auto',
+    baseline=None, restore_best_weights=False
+)
+
+for beta in np.concatenate((np.logspace(-4,np.log10(7),20),
+               np.logspace(np.log10(7),-5,20)[1:],
+                np.logspace(-5,np.log10(7),20)[1:])):
+    modelcheckpoint = keras.callbacks.ModelCheckpoint(train_output_dir + '/model_weights_{epoch:02d}.hdf5', save_freq = save_period*steps_per_epoch, save_weights_only=True)
+    callbacks=[tf.keras.callbacks.CSVLogger(train_output_dir + '/log.csv', separator=",", append=True),
+            reduceLR,earlystop,
+            modelcheckpoint,
+            reset_metrics_inst]
+    vae.beta.assign(beta)
+    K.set_value(vae.optimizer.lr,3e-5)
+    
+    my_history = vae.fit(x=train_x, y=train_y, batch_size=batch_size,
+                epochs=10000,verbose=1,
+                validation_data = (valid_x[:200*batch_size],valid_y[:200*batch_size]),
+                callbacks = callbacks,
+                initial_epoch=init_epoch,
+                steps_per_epoch = steps_per_epoch
+              )
+    init_epoch = my_history.epoch[-1]
+    vae.save_weights(train_output_dir + '/model_weights_end_' + str(init_epoch) + '_' + "{:.1e}".format(beta) + '.hdf5')
+
+
+# In[ ]:
+
+
+# init_epoch = 544
+steps_per_epoch = 1000
+save_period = 10
+
+
+reduceLR = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), patience=5, verbose=1, mode='auto', min_delta=1e-4, cooldown=0, min_lr=1e-8)
+earlystop = tf.keras.callbacks.EarlyStopping(
+    monitor='val_loss', min_delta=0., patience=10, verbose=0, mode='auto',
+    baseline=None, restore_best_weights=False
+)
+
+
+for beta in np.concatenate((np.logspace(-4,np.log10(0.7),15),
+               np.logspace(np.log10(0.7),-5,20)[1:],
+                np.logspace(-5,np.log10(0.7),20)[1:])):
+    modelcheckpoint = keras.callbacks.ModelCheckpoint(train_output_dir + '/model_weights_{epoch:02d}.hdf5', save_freq = save_period*steps_per_epoch, save_weights_only=True)
+    callbacks=[tf.keras.callbacks.CSVLogger(train_output_dir + '/log.csv', separator=",", append=True),
+            reduceLR,earlystop,
+            modelcheckpoint,
+            reset_metrics_inst]
+    vae.beta.assign(beta)
+    K.set_value(vae.optimizer.lr,3e-5)
+    
+    my_history = vae.fit(x=train_x, y=train_y, batch_size=batch_size,
+                epochs=10000,verbose=1,
+                validation_data = (valid_x[:200*batch_size],valid_y[:200*batch_size]),
+                callbacks = callbacks,
+                initial_epoch=init_epoch,
+                steps_per_epoch = steps_per_epoch
+              )
+    init_epoch = my_history.epoch[-1]
+    vae.save_weights(train_output_dir + '/model_weights_end_' + str(init_epoch) + '_' + "{:.1e}".format(beta) + '.hdf5')
+
